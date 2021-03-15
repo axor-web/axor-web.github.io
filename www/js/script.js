db20db70eaca5b6cf99f15313a7841fc263864da
Алексей <axor@MacBook-Pro-Aleksej.local>
2021-03-15 21:57:34 +0500

    added testimonials cards

--- www/js/script.js
+++ www/js/script.js
@@ -1,3 +1,5 @@
+"use strict"
+
 // Скрипт для поиска
 
 let searchButton = document.querySelector('.header__buttons-search img');
@@ -93,3 +95,104 @@ function makeParallax(layers, mobileWidth) {
 
 makeParallax(document.querySelectorAll('.main__hero-layer'), 1100)
 makeParallax(document.querySelectorAll('.main__documentation-layer'), 1100)
+
+
+function makeSlider(block, perspective, depth) {
+    let style = document.querySelector('style');
+    let cards = document.querySelectorAll(block + '-card');
+    let last = cards.length - 1;
+
+    cards[0].parentNode.style.perspective = perspective;
+
+    for (let i = 0; i <= last; i++) {
+        style.textContent += `
+            .main__testimonials-info-card[data-pos="${i}"] {
+                z-index: ${100 - i};
+                transform: translateZ(-${i * depth}vw);
+            }
+        `;
+    }
+
+    style.textContent += `
+        @keyframes leaf-card {
+            from {
+                z-index: ${100 - last};
+                transform: translateZ(-${last * depth}vw);
+            }
+            
+            50% {
+                z-index: 100;
+                transform: translateZ(-${last/2 * depth}vw) translateY(-150%);
+            }
+            
+            to {
+                z-index: 100;
+                transform: translateZ(0) translateY(0);
+            }
+        }
+
+        .leaf {
+            animation: leaf-card .3s ease-in 1 normal;
+        }
+        .leaf-back {
+            animation: leaf-card .3s ease-in 1 reverse;
+        }
+
+        ${block}-card {
+            transition: transform .3s, z-index .3s;
+        }
+    `;
+
+    let arrows = document.querySelectorAll(block + '-arrows a');
+
+    let num = 0;
+    function leafCard(count) {
+        //чекаем допустим ли num и меняем состояние стрелочек
+        num += count;
+
+        if (num < 0)     { num = 0; return false }
+        if (num == last + 1) { num = last; return false }
+
+        // меняем вид стрелочек
+        arrows[0].classList.remove('arrow_inactive');
+        arrows[1].classList.remove('arrow_inactive');
+        if (num == 0)        { arrows[0].classList.add('arrow_inactive'); }
+        if (num == last) { arrows[1].classList.add('arrow_inactive'); }
+
+        //меняем позиции элементов
+        if (count + 1) {
+            for (let card of cards) {
+                let pos = +card.getAttribute('data-pos');
+
+                if (pos == last) {
+                    card.setAttribute('data-pos', '0');
+                    card.classList.add('leaf');
+                    setTimeout(() => card.classList.remove('leaf'), 300);
+                }
+
+                else { card.setAttribute('data-pos', `${++pos}`); }
+            }
+        }
+
+        else {
+            for (let card of cards) {
+                let pos = +card.getAttribute('data-pos');
+
+                if (pos == 0) {
+                    card.setAttribute('data-pos', `${last}`);
+                    card.classList.add('leaf-back');
+                    setTimeout(() => card.classList.remove('leaf-back'), 300);
+                }
+
+                else { card.setAttribute('data-pos', `${--pos}`); }
+            }
+        }
+    }
+
+    //back
+    arrows[0].addEventListener('click', leafCard.bind(null, -1));
+    //forward
+    arrows[1].addEventListener('click', leafCard.bind(null, 1));
+}
+
+makeSlider('.main__testimonials-info', '15vw', 1.3);
