291fae91f08ec21ee7b414570d52ddad48d0b729
Alexey <axor@MacBook-Pro-Aleksej.local>
2021-05-01 22:46:09 +0500

    fix bugs with visibility

--- www/js/script.js
+++ www/js/script.js
@@ -5,35 +5,36 @@
 let searchButton = document.querySelector('.header__buttons-search img');
 let searchInput = document.querySelector('.header__buttons-search input[type="text"]');
 let searchParent = searchInput.parentNode;
-const hiddenClass= 'header__buttons-search_hidden';
+const visibleClass= 'header__buttons-search_visible';
 
 function search(value) {
     if (value) {
         window.open(`https://www.google.com/search?q=${value}`, '_blank');
         searchInput.value = '';
         searchInput.blur();
-        searchParent.classList.add(hiddenClass);
+        searchParent.classList.remove(visibleClass);
     }
 }
 
 searchButton.addEventListener('click', () => {
-    if ([].includes.call(searchParent.classList, hiddenClass)) { // заимствуем у массива метод includes для classList
-        searchParent.classList.remove(hiddenClass);
+    if (![].includes.call(searchParent.classList, visibleClass)) { // заимствуем у массива метод includes для classList
+        searchParent.classList.add(visibleClass);
         searchButton.blur();
         searchInput.focus();
         searchInput.classList.add('focused');
     }
-    else search(searchInput.value);
+    else if (searchInput.value) { search(searchInput.value); }
 });
 
 searchInput.addEventListener('blur', () => {
-    if (![].includes.call(searchParent.classList, hiddenClass) && !searchInput.value) {
-        searchParent.classList.add(hiddenClass);
+    if ([].includes.call(searchParent.classList, visibleClass) && !searchInput.value) {
+        console.log('wow!');
+        searchParent.classList.remove(visibleClass);
         searchInput.classList.remove('focused');
     }
 });
 searchInput.addEventListener('keyup', event => {
-    if (event.code == 'Enter') {
+    if (event.code == 'Enter' && searchInput.value) {
         search(searchInput.value);
     }
 });
@@ -48,13 +49,13 @@ let mobileButton = document.querySelector('.header__mobile-button');
 mobileButton.addEventListener('click', () => {
     let header = document.querySelector('.header');
     
-    if (![].includes.call(header.classList, 'header_hidden')) {
-        header.classList.add('header_hidden');
-        document.body.style.overflowY = '';
+    if (![].includes.call(header.classList, 'header_visible')) {
+        header.classList.add('header_visible');
+        document.body.style.overflowY = 'hidden';
     } 
     else {
-        header.classList.remove('header_hidden');
-        document.body.style.overflowY = 'hidden';
+        header.classList.remove('header_visible');
+        document.body.style.overflowY = '';
     }
 });
 
