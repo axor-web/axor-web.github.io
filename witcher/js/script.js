38797688e745af861ead02bf3ca30b9528bdb171
Alexey <=>
2021-05-31 15:44:32 +0500

    new feature at header

--- witcher/js/script.js
+++ witcher/js/script.js
@@ -1,12 +1,18 @@
-let button = document.querySelector('.header__button');
-let header = button.parentNode;
+let button      = document.querySelector('.header__button');
+let header      = button.parentNode;
+let mobileWidth = 600;
 
-button.addEventListener('click', (e) => {
-    e.preventDefault();
-    
-    if ([].includes.call(header.classList, 'header_visible')) {
-        header.classList.remove('header_visible');
-        return null;
+function openHeader(e) {
+    if (window.innerWidth <= mobileWidth) {
+        e.preventDefault();
+
+        if ([].includes.call(header.classList, 'header_visible')) {
+            header.classList.remove('header_visible');
+            return null;
+        }
+        header.classList.add('header_visible');
     }
-    header.classList.add('header_visible');
-})
+}
+
+button.addEventListener('click', openHeader);
+header.addEventListener('mouseleave', openHeader);
