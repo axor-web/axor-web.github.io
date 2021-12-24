9a8d682e6849ee473e8d3cbd83a9cf92e24e88f3
Alexey <axor.webdev@gmail.com>
2021-12-24 18:48:01 +0500

    made computer version of index

---
+++ kvartirus/scripts/index.js
@@ -0,0 +1,26 @@
+const btn = document.querySelector('.header__menu-button');
+
+function showMenu() {
+    const menu = document.querySelector('.header__menu')
+    const card = document.querySelector('.header__menu-card');
+
+    if ([].includes.call(menu.classList, 'header__menu_active')) {
+        menu.classList.remove('header__menu_active');
+    }
+    else {
+        menu.classList.add('header__menu_active');
+    }
+}
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    showMenu();
+})
+
+
+const search = document.querySelector('.search')
+
+search.onclick = () => {
+    search.classList.add('search_active');
+    search.onclick = null;
+};
