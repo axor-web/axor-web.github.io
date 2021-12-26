2a5013468bd1ba049346211d429f28608bf0d7e5
Alexey <axor.webdev@gmail.com>
2021-12-27 02:19:16 +0500

    test perfomance

--- kvartirus/scripts/search-results-slider.js
+++ kvartirus/scripts/search-results-slider.js
@@ -126,7 +126,7 @@ const bedsNumControls = document.querySelector('#beds-num ~ .controls');
 countSomething(bedsNum, bedsNumControls);
 
 window.onload = () => {
-    document.querySelector('.main__specs-menu-map iframe').src = "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15051.073200557083!2d37.58078374853958!3d55.728877795253304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sru!4v1640435531733!5m2!1sru!2sru"
+    //document.querySelector('.main__specs-menu-map iframe').src = "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15051.073200557083!2d37.58078374853958!3d55.728877795253304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sru!4v1640435531733!5m2!1sru!2sru"
 }
 
 
