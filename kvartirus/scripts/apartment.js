5f97b2caf5a9309845d9d40b6c65adbb81f31160
Alexey <axor.webdev@gmail.com>
2021-12-29 20:52:47 +0500

    add masonry for benefits

--- kvartirus/scripts/apartment.js
+++ kvartirus/scripts/apartment.js
@@ -31,3 +31,41 @@ window.onscroll = () => {
     document.querySelector('.main__map iframe').src = "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15051.073200557083!2d37.58078374853958!3d55.728877795253304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sru!4v1640435531733!5m2!1sru!2sru";
     window.onscroll = null;
 }
+
+
+/*function sortBenefits(listElem) {
+    arr = [...listElem.children];
+    arr.sort((elem1, elem2) => {
+        if (elem1.clientHeight > elem2.clientHeight)      { return 1;  }
+        else if (elem1.clientHeight < elem2.clientHeight) { return -1; }
+        return 0;
+    });
+    arr.reverse();
+
+    for (let i = 1; i <= arr.length; i++) {
+        elem = arr[i-1];
+        elem.style.order = i;
+    }
+}
+
+sortBenefits(document.querySelector('.main__benefits-list'));*/
+
+const list = document.querySelector('.main__benefits-list');
+
+const masonry = Macy({
+    container: list,
+    margin: {
+        x: 8,
+        y: 8
+    },
+    columns: list.children.length,
+    breakAt: {
+        1210: 5,
+        1015: 4,
+        810: 3,
+        620: 2,
+        420: 1
+    }
+});
+
+window.onresize = masonry.recalculate.bind(masonry);
