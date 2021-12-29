fa4ba2d67bb705ea4d14437d3101d8eb79919c7b
Alexey <axor.webdev@gmail.com>
2021-12-29 18:58:12 +0500

    fix some bugs

--- kvartirus/scripts/apartment.js
+++ kvartirus/scripts/apartment.js
@@ -1,3 +1,22 @@
+const btn = document.querySelector('.header__menu-button');
+
+function showMenu() {
+    const menu = document.querySelector('.header__menu')
+    const card = document.querySelector('.header__menu-card');
+
+    if ([].includes.call(menu.classList, 'header__menu_active')) {
+        menu.classList.remove('header__menu_active');
+    }
+    else {
+        menu.classList.add('header__menu_active');
+    }
+}
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    showMenu();
+})
+
 const slider = new Slider(document.querySelector('.slider'), 
 [
     'apartment1',
@@ -8,6 +27,7 @@ const slider = new Slider(document.querySelector('.slider'),
 ]);
 
 
-window.onload = () => {
-    setTimeout(() => document.querySelector('.main__map iframe').src = "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15051.073200557083!2d37.58078374853958!3d55.728877795253304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sru!4v1640435531733!5m2!1sru!2sru", 100);
+window.onscroll = () => {
+    document.querySelector('.main__map iframe').src = "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15051.073200557083!2d37.58078374853958!3d55.728877795253304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sru!4v1640435531733!5m2!1sru!2sru";
+    window.onscroll = null;
 }
