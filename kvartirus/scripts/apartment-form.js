03819eec5c976ab7b760c0c6dc6c61d7838e19ec
Alexey <axor.webdev@gmail.com>
2022-01-05 14:04:00 +0500

    fix bugs on ios

--- kvartirus/scripts/apartment-form.js
+++ kvartirus/scripts/apartment-form.js
@@ -71,3 +71,8 @@ window.onclick = () => {
     document.querySelector('.main__forms-check-map-iframe iframe').src = "https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15051.073200557083!2d37.58078374853958!3d55.728877795253304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sru!4v1640435531733!5m2!1sru!2sru";
     window.onclick = null;
 }
+
+let inputs = document.querySelectorAll('input[readonly]');
+for (let input of inputs) {
+    input.onclick = function() { this.blur(); }
+}
